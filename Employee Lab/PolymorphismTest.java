01375f53a7d474a9441869deb169fd9153840e3c
jak_shaon <106922308+jak-Shaon@users.noreply.github.com>
2024-01-03 23:52:58 +0600

    Add files via upload

---
+++ Employee Lab/PolymorphismTest.java
@@ -0,0 +1,17 @@
+public class PolymorphismTest {
+    public static void main(String[] args) {
+        CommissionEmployee commissionEmployee = new CommissionEmployee(
+            "Faraha Ulfath", "Khan", "221-134-030", 10000, .06);
+            
+        BasePlusCommissionEmployee BasePlusCommissionEmployee = new BasePlusCommissionEmployee(
+            "Faraha Ulfath", "Khan", "221-134-030", 10000, .06,300);
+
+        System.out.printf("%s %s:%n%n%s%n%n","Call Commission's toString with superclass reference ","to superclass object", commissionEmployee.toString());
+
+        System.out.printf("%s %s:%n%n%s%n%n","Call Commission's toString with superclass reference ","to superclass object", BasePlusCommissionEmployee.toString());
+
+        BasePlusCommissionEmployee commissionEmployee2 = BasePlusCommissionEmployee;
+        System.out.printf("%s %s:%n%n%s%n%n","Call Commission's toString with superclass reference ","to superclass object", commissionEmployee2.toString());
+           
+    }
+}
---
+++ Employee Lab/SalariedEmployee.java
@@ -0,0 +1,34 @@
+// Fig. 10.5: SalariedEmployee.java
+
+public class SalariedEmployee extends Employee {
+    private double weeklySalary;
+    public SalariedEmployee(String firstName, String lastName,String socialSecurityNumber, double weeklySalary)
+    {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (weeklySalary <0.0)
+        throw new IllegalArgumentException("Weekly Salary must be >= 0.0");
+    }
+
+    public void setWeeklySalary(double weeklySalary)
+    {
+        if (weeklySalary <0.0)
+        throw new IllegalArgumentException("Weekly Salary must be >= 0.0");
+    }
+
+    public double getWeeklySalary()
+    {
+        return this.weeklySalary;
+    }
+    @Override
+    public String toString()
+    {
+        return String.format("Salaried Employee: %s%n%s: $%,.2f", super.toString(), "Weekly salary", getWeeklySalary());
+    }
+
+    @Override
+    public double earning() 
+    {
+        return 0;
+    }
+}
